25b0f08fa58eb120cb40c3286a21247659d95f7e
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-20 16:14:37 -0400

    add tests

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ var path = require('path');
 var File = require('vinyl');
 var vfs = require('vinyl-fs');
 var assert = require('assert');
+var through = require('through2');
 var tree = require('./');
 
 var cwd = path.resolve.bind(path, __dirname, 'fixtures');
@@ -15,9 +16,52 @@ function isMatch(re, file) {
 }
 
 describe('tree()', function() {
-  it.only('should create tree.txt', function(cb) {
+  it('should create a file tree', function(cb) {
     var buffer = [];
     var stream = vfs.src('**/*.*', {cwd: cwd(), dot: true})
+      .pipe(tree.create())
+      .on('data', function(file) {
+        buffer.push(file);
+      })
+      .on('end', function() {
+        assert.equal(buffer.length, 1);
+        assert.equal(buffer[0].basename, 'tree.txt');
+        assert(isMatch(/fixtures/, buffer[0]));
+        assert(isMatch(/index\.ejs/, buffer[0]));
+        assert(isMatch(/index\.hbs/, buffer[0]));
+        cb();
+      });
+  });
+
+  it('should create a file tree with modified file paths', function(cb) {
+    var buffer = [];
+    var stream = vfs.src('**/*.*', {cwd: cwd(), dot: true})
+      .pipe(through.obj(function(file, enc, next) {
+        file.path = path.join(path.dirname(file.path), path.basename(file.path, path.extname(file.path)) + '.foo');
+        next(null, file);
+      }))
+      .pipe(tree.create())
+      .on('data', function(file) {
+        buffer.push(file);
+      })
+      .on('end', function() {
+        assert.equal(buffer.length, 1);
+        assert.equal(buffer[0].basename, 'tree.txt');
+        assert(isMatch(/fixtures/, buffer[0]));
+        assert(isMatch(/index\.foo/, buffer[0]));
+        assert(isMatch(/index\.foo/, buffer[0]));
+        cb();
+      });
+  });
+
+  it('should create a file tree with unmodified file paths', function(cb) {
+    var buffer = [];
+    var stream = vfs.src('**/*.*', {cwd: cwd(), dot: true})
+      .pipe(tree.capture())
+      .pipe(through.obj(function(file, enc, next) {
+        file.path = path.join(path.dirname(file.path), path.basename(file.path, path.extname(file.path)) + '.foo');
+        next(null, file);
+      }))
       .pipe(tree.create())
       .on('data', function(file) {
         buffer.push(file);
